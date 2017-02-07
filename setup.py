44d9348d5db84d88099699bfe4459310e10c48bc
Aleksey Cherepanov <ftp27host@gmail.com>
2017-02-07 18:07:05 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+# Parse the version from the mapbox module.
+with open('taigacli/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+setup(name='taigacli',
+      version=version,
+      description="Command line interface to Taiga",
+      classifiers=[],
+      keywords='',
+      author="Alksey Cherepanov",
+      author_email='ftp27host@gmail.com',
+      url='https://github.com/Indev-Group/taiga_cli',
+      license='MIT',
+      install_requires=[
+          'terminaltables',
+          'python-taiga',
+          'click',
+          'click-plugins']
+)
---
+++ taigacli/__init__.py
@@ -0,0 +1,3 @@
+# Command line interface to Taiga
+
+__version__ = '0.1.0'
